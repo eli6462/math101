22c8323ead25655878684d5cf84b0f77f1f40784
eli eliseiko <eliseiko64@gmail.com>
2022-03-14 21:14:05 +0200

    fake button added

--- WindowsFormsApp3 (matrix calculator)/Form1.Designer.cs
+++ WindowsFormsApp3 (matrix calculator)/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace WindowsFormsApp3__matrix_calculator_
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnSuperClick = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -51,11 +52,21 @@ namespace WindowsFormsApp3__matrix_calculator_
             this.btnSuperClick.Text = "button1";
             this.btnSuperClick.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(327, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnSuperClick);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
@@ -68,6 +79,7 @@ namespace WindowsFormsApp3__matrix_calculator_
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnSuperClick;
+        private System.Windows.Forms.Button button1;
     }
 }
 
