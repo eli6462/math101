2b0d8927de0b5c83ac6aa49b258a32f192d6a549
eli6462 <eliseiko64@gmail.com>
2022-03-17 18:32:55 +0200

    Archenicture update

--- WindowsFormsApp3 (matrix calculator)/MatrixUserControl1.cs
+++ WindowsFormsApp3 (matrix calculator)/MatrixUserControl1.cs
@@ -10,15 +10,36 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp3__matrix_calculator_
 {
-    public partial class Form2 : Form
+    public partial class UserControl1 : UserControl
     {
-        private static int[,] matrixA = new int[3, 3];
-        public Form2()
+        public UserControl1()
         {
             InitializeComponent();
         }
 
+        private static int[,] matrixA = new int[3, 3];
+        private static int[,] matrixB = new int[3, 3];
+        private static int[,] resultmatrix = new int[3, 3];
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //initMatrix(groupBox2, matrixA);
+            initMatrixTEST(groupBox3, matrixA);
 
+            //test
+
+            for (int i = 0; i < matrixA.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixA.GetLength(1); j++)
+                {
+                    testTextBox.Text += matrixA[i, j];
+                }
+                testTextBox.Text += "\n n ";
+            }
+
+            // resultMatrix = MatrixHandler.combineMatrixes(matrixA, matrixB);
+
+            // print result matrix to user FUNC
+        }
 
         // loop thru textboxes
         private void initMatrix(GroupBox gBox, int[,] arr2d)
@@ -27,16 +48,16 @@ namespace WindowsFormsApp3__matrix_calculator_
 
             foreach (Control c in gBox.Controls)
             {
-                if (c is TextBox)                                    
+                if (c is TextBox)
                 {
-                    arr2d[i,j] = int.Parse(c.Text);
+                    arr2d[i, j] = int.Parse(c.Text);
 
                     // this code loops thru all indexes of arr2d (all possible i AND j combinations)
-                    if (j < arr2d.GetLength(1)-1 && i <= arr2d.GetLength(0)-1)
+                    if (j < arr2d.GetLength(1) - 1 && i <= arr2d.GetLength(0) - 1)
                     {
                         j++;
                     }
-                    else if (j >= arr2d.GetLength(1)-1 && i <= arr2d.GetLength(0)-2)
+                    else if (j >= arr2d.GetLength(1) - 1 && i <= arr2d.GetLength(0) - 2)
                     {
                         j = 0;
                         i++;
@@ -48,7 +69,7 @@ namespace WindowsFormsApp3__matrix_calculator_
         }
 
         private void initMatrixTEST(GroupBox gBox, int[,] arr2d)
-        {            
+        {
 
             foreach (Control c in gBox.Controls)
             {
@@ -59,33 +80,18 @@ namespace WindowsFormsApp3__matrix_calculator_
                     for (int i = 0; i < arr2d.GetLength(0); i++)
                     {
                         for (int j = 0; j < arr2d.GetLength(1); j++)
-                        {                            
-                            if (c.Name == ("resultMatrix" + (i+1).ToString()+"_"+ (j + 1).ToString()))
-                            {                                
+                        {
+                            if (c.Name == ("resultMatrix" + (i + 1).ToString() + "_" + (j + 1).ToString()))
+                            {
                                 arr2d[i, j] = int.Parse(c.Text);
                             }
                         }
                     }
-                                       
+
                 }
             }
         }
 
-        private void Form2_Load(object sender, EventArgs e)
-        {
-            
-        }      
 
-        private void btnMatrixInterface_Click(object sender, EventArgs e)
-        {
-            this.userControl11.Show();
-            this.funcUserControl11.Hide();
-        }
-
-        private void btnFunctionInterface_Click(object sender, EventArgs e)
-        {
-            this.userControl11.Hide();
-            this.funcUserControl11.Show();
-        }
     }
 }
